7bfbe90cc2a27f06412e1b23caeb8557fcc0f885
Naim_Islam2 <1931361@iub.edu.bd>
2024-03-11 09:58:18 +0000

    3rd Edit

--- lib/location.dart
+++ lib/location.dart
@@ -19,23 +19,59 @@ class LocationPage extends StatelessWidget {
               ),
             ),
             SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Navigate to Dhanmondi page
-              },
-              child: Text('Dhanmondi'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    // Navigate to Dhanmondi page
+                  },
+                  child: Text('Dhanmondi'),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    // Navigate to Dhanmondi directions
+                  },
+                  child: Text('Directions'),
+                ),
+              ],
             ),
-            ElevatedButton(
-              onPressed: () {
-                // Navigate to Banani page
-              },
-              child: Text('Banani'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    // Navigate to Banani page
+                  },
+                  child: Text('Banani'),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    // Navigate to Banani directions
+                  },
+                  child: Text('Directions'),
+                ),
+              ],
             ),
-            ElevatedButton(
-              onPressed: () {
-                // Navigate to Mohammadpur page
-              },
-              child: Text('Mohammadpur'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    // Navigate to Mohammadpur page
+                  },
+                  child: Text('Mohammadpur'),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    // Navigate to Mohammadpur directions
+                  },
+                  child: Text('Directions'),
+                ),
+              ],
             ),
             SizedBox(height: 20),
             ElevatedButton(
